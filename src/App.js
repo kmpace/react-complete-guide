b4e79d75ecfe65360c0b81b0e0f4fd02818f4f04
Korey Pace <korey.pace@gmail.com>
2018-03-07 15:50:04 +0100

    vscode test

--- src/App.js
+++ src/App.js
@@ -53,7 +53,8 @@ class App extends Component {
   render() {
 
     const style = {  //in-line styles 
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1x solid blue',
       padding: '8px',
@@ -75,6 +76,8 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = "red"; //asssigning a style in the if statement 
     }
 
     return (
